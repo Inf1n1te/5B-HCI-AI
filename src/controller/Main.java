8f2cee58e1424356cd82da77908a5b69d7ca7c03
basUT <basj95@hotmail.com>
2014-11-24 16:44:45 +0100

    Final so far

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,9 +1,11 @@
 package controller;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import tokenization.Trainer;
 import model.Words;
 
 public class Main {
@@ -11,7 +13,7 @@ public class Main {
 	private static float k = 1;
 	private static Words words = new Words();
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		System.out.print("Enter value for k:\n");
 		try {
--- src/tokenization/Token.java
+++ src/tokenization/Token.java
@@ -5,65 +5,59 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Arrays;
 
 public class Token {
 
-	// str
-
-	private InputStream input;
-	private String s;
-	private String tokenized;
-	private String[] token;
-	private String[] tokenizedArray;
-	private String everything;
-	private static File file;
-
+// str
+	
+private String everything;
+private static File file;
+	
+	
 	public Token(File path) throws FileNotFoundException, IOException {
 		getInput(path);
 		getInputConsole();
 	}
-
-	public void getInputConsole() throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print("Enter String\n");
-		String s = br.readLine();
-		tokenize(s);
+	
+	public void getInputConsole() throws IOException{
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        System.out.print("Enter String\n");
+	        String s = br.readLine();
+	        tokenize(s);
 	}
+	
+	
+	public void getInput(File f) throws FileNotFoundException, IOException{
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(f))) {
+	        StringBuilder sb = new StringBuilder();
+	        String line = br.readLine();
 
-	public void getInput(File f) throws FileNotFoundException, IOException {
-
-		try (BufferedReader br = new BufferedReader(new FileReader(f))) {
-			StringBuilder sb = new StringBuilder();
-			String line = br.readLine();
-
-			while (line != null) {
-				sb.append(line);
-				sb.append(System.lineSeparator());
-				line = br.readLine();
-			}
-			everything = sb.toString();
-		}
-
+	        while (line != null) {
+	            sb.append(line);
+	            sb.append(System.lineSeparator());
+	            line = br.readLine();
+	        }
+	        everything = sb.toString();
+	    }
+		
+		
 		tokenize(everything);
 	}
 
-	public static String[] tokenize(String c) {
-		String[] tokenized = c.replaceAll("[^a-zA-Z ]", "").toLowerCase()
-				.split("\\s+");
-		// String[] tokenized = c.split(" ").toLowerCase();
-//		System.out.println(Arrays.toString(tokenized));
+	public static String[] tokenize(String c){
+		String[] tokenized = c.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
+		System.out.println(Arrays.toString(tokenized));
 		return tokenized;
+		
 	}
-
-	public static void main(String[] args) throws FileNotFoundException,
-			IOException {
+		
+	
+	/*public static void main(String[] args) throws FileNotFoundException, IOException{
 		file = new File("C:/Universiteit/M-train5.txt");
 		new Token(file);
-	}
-
+	}*/
+	
 }
