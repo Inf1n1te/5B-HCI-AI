a15e4a8237440f05d0a5152b710cb02934f9d01a
Inf1n1te <timkerkhoven@live.nl>
2014-11-24 15:11:15 +0100

    stuff

--- src/model/Word.java
+++ src/model/Word.java
@@ -1,12 +1,12 @@
 package model;
 
 public class Word {
-	
+
 	private String word;
 	private int maleCount = 0;
 	private int femaleCount = 0;
-	private float probMale;
-	private float probFemale;
+	private double probMale;
+	private double probFemale;
 
 	public Word(String word, boolean male) {
 		this.word = word;
@@ -16,20 +16,39 @@ public class Word {
 			femaleCount++;
 		}
 	}
-	
-	public void incrementMale() {
-		maleCount++;
-	}
-	
-	public void incrementFemale() {
-		femaleCount++;
+
+	public void increment(boolean male) {
+		if (male) {
+			maleCount++;
+		} else {
+			femaleCount++;
+		}
 	}
 
-	public float getProbMale() {
+	public String getWord() {
+		return word;
+	}
+
+	public double getProbMale() {
 		return probMale;
 	}
-	
-	public float getProbFemale() {
+
+	public double getProbFemale() {
 		return probFemale;
+	}
+
+	public void calculateSmoothProbs(int vocab, int k, int female, int male) {
+		if (femaleCount == 0) {
+			probFemale = 0;
+		} else {
+			probFemale = Math.log((femaleCount + k) / (female + k * vocab))
+					/ Math.log(2);
+		}
+		if (femaleCount == 0) {
+			probMale = 0;
+		} else {
+			probMale = Math.log((maleCount + k) / (male + k * vocab))
+					/ Math.log(2);
+		}
 	}
 }
--- src/model/Words.java
+++ src/model/Words.java
@@ -3,7 +3,7 @@ package model;
 import java.util.ArrayList;
 
 public class Words {
-	
+
 	private int maleCount = 0;
 	private int femaleCount = 0;
 	private ArrayList<Word> words;
@@ -11,15 +11,35 @@ public class Words {
 	public Words() {
 		words = new ArrayList<Word>();
 	}
-	
+
 	public void addWord(String word, boolean male) {
 		if (male) {
 			maleCount++;
 		} else {
 			femaleCount++;
 		}
+		boolean contains = false;
 		for (int i = 0; i < words.size(); i++) {
-			
+			if (words.get(i).getWord().equals(word)) {
+				words.get(i).increment(male);
+				contains = true;
+				break;
+			}
+		}
+		if (contains == false) {
+			words.add(new Word(word, male));
+		}
+	}
+	
+	public void calculateProbs() {
+		for (int i = 0; i < words.size(); i++) {
+			words.get(i).calculateSmoothProbs(0, 0, femaleCount, maleCount);
+		}
+	}
+	
+	public void calculateSmoothProbs(int k) {
+		for (int i = 0; i < words.size(); i++) {
+			words.get(i).calculateSmoothProbs(words.size(), k, femaleCount, maleCount);
 		}
 	}
 
